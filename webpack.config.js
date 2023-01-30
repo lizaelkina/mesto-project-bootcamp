5923e66fea7542ceece26c4f374a3db9304abfeb
Liza Petkova <lizapetkova@yandex.ru>
2023-01-31 02:14:56 +0300

    feat: настроить Webpack для html

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 module.exports = {
   entry: {main: './src/index.js'},
@@ -15,12 +17,21 @@ module.exports = {
     open: true
   },
   module: {
-    rules: [
+    rules: [{
+      test: /\.js$/,
+      use: 'babel-loader',
+      exclude: '/node_modules/'
+    },
       {
-        test: /\.js$/,
-        use: 'babel-loader',
-        exclude: '/node_modules/'
-      }
+        test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+        type: 'asset/resource'
+      },
     ]
-  }
-}
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
+    new CleanWebpackPlugin(),
+  ]
+};
