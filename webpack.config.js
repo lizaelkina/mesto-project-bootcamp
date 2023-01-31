2c60e0864844426558f65bad1a07f83a6bf4a93b
Liza Petkova <lizapetkova@yandex.ru>
2023-01-31 15:18:36 +0300

    feat: настроить минификацию CSS и добавление префиксов

--- webpack.config.js
+++ webpack.config.js
@@ -31,8 +31,13 @@ module.exports = {
       {
         test: /\.css$/,
         use: [MiniCssExtractPlugin.loader, {
-          loader: 'css-loader'
-        }]
+          loader: 'css-loader',
+          options: {
+            importLoaders: 1
+          }
+        },
+          'postcss-loader'
+        ]
       }
     ]
   },
