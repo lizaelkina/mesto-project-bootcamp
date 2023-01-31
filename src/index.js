0b2f61208889b7e7a1255d5f257893ec781626b1
Liza Petkova <lizapetkova@yandex.ru>
2023-01-31 14:50:43 +0300

    feat: настроить Webpack для CSS

--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
 console.log('Hello, World!');
+
+import './pages/index.css';
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: {main: './src/index.js'},
@@ -17,15 +18,22 @@ module.exports = {
     open: true
   },
   module: {
-    rules: [{
-      test: /\.js$/,
-      use: 'babel-loader',
-      exclude: '/node_modules/'
-    },
+    rules: [
       {
-        test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: '/node_modules/'
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|woff(2)?|eot|ttf|otf)$/,
         type: 'asset/resource'
       },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, {
+          loader: 'css-loader'
+        }]
+      }
     ]
   },
   plugins: [
@@ -33,5 +41,6 @@ module.exports = {
       template: './src/index.html'
     }),
     new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin()
   ]
 };
