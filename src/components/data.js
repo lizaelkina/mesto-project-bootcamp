7eecf58c38190f8eb0f9c70d2ecc071e894b827a
Liza Petkova <lizapetkova@yandex.ru>
2023-01-31 13:50:41 +0300

    feat: подготовить изображения для сборки

---
+++ src/components/data.js
@@ -0,0 +1,33 @@
+// import spb from '/src/images/spb.jpeg';
+// import moscow from '/src/images/moscow.jpeg';
+// import kazan from '/src/images/kazan.jpeg';
+// import vyborg from '/src/images/vyborg.jpeg';
+// import pskov from '/src/images/pskov.jpeg';
+// import korela from '/src/images/korela.jpeg';
+//
+// const galleryCards = [
+//   {
+//     name: 'Санкт-Петербург',
+//     link: spb
+//   },
+//   {
+//     name: 'Москва',
+//     link: moscow
+//   },
+//   {
+//     name: 'Казань',
+//     link: kazan
+//   },
+//   {
+//     name: 'Выборг',
+//     link: vyborg
+//   },
+//   {
+//     name: 'Псков',
+//     link: pskov
+//   },
+//   {
+//     name: 'Приозерск',
+//     link: korela
+//   }
+// ];
