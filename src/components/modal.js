9f3a86ae249b41ae3f6db162a0b7a7d51f723a66
Liza Petkova <lizapetkova@yandex.ru>
2023-02-01 13:38:24 +0300

    refactor: оставить в modal.js только общие функции открытия и закрытия модальных окон, остальной функционал перенести в index.js

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,6 +1,4 @@
-import {configSelectorForm, content, galleryCardList, page} from './utils';
-import {createCardElement} from './card';
-import {hideInputError, toggleButtonState} from './validate';
+import {page} from './utils';
 
 // открытие попапов
 
@@ -35,79 +33,4 @@ popupList.forEach(popup => {
   });
 })
 
-// попап редактирования профиля
-
-const popupProfile = page.querySelector('.popup_type_edit-profile');
-const buttonEditProfile = content.querySelector('.profile__btn-edit');
-const formSaveProfile = popupProfile.querySelector('.form');
-const profileNameInput = formSaveProfile.querySelector('.form__input_type_profile-name');
-const profileDescriptionInput = formSaveProfile.querySelector('.form__input_type_profile-description');
-const profileNameErrorElement = formSaveProfile.querySelector('.profile-name-error');
-const profileDescriptionErrorElement = formSaveProfile.querySelector('.profile-description-error');
-const buttonSaveProfile = popupProfile.querySelector('.form__btn-save')
-const profileNameElement = content.querySelector('.profile__title');
-const profileDescriptionElement = content.querySelector('.profile__subtitle');
-
-
-function openProfilePopup() {
-  profileNameInput.value = profileNameElement.textContent;
-  profileDescriptionInput.value = profileDescriptionElement.textContent;
-  hideInputError(profileNameInput, profileNameErrorElement, configSelectorForm);
-  hideInputError(profileDescriptionInput, profileDescriptionErrorElement, configSelectorForm);
-  toggleButtonState(buttonSaveProfile, false, configSelectorForm);
-  openPopup(popupProfile);
-}
-
-function saveProfile(event) {
-  event.preventDefault();
-  const name = profileNameInput.value;
-  const description = profileDescriptionInput.value;
-  profileNameElement.textContent = name;
-  profileDescriptionElement.textContent = description;
-  closePopup(popupProfile);
-}
-
-// попап добавления изображений
-
-const popupAddPhoto = page.querySelector('.popup_type_add-photo');
-const buttonAddPhoto = content.querySelector('.profile__btn-add');
-const formAddPhoto = popupAddPhoto.querySelector('.form');
-const photoNameInput = popupAddPhoto.querySelector('.form__input_type_photo-name');
-const photoUrlInput = popupAddPhoto.querySelector('.form__input_type_photo-url');
-const photoNameErrorElement = popupAddPhoto.querySelector('.photo-name-error');
-const photoUrlErrorElement = popupAddPhoto.querySelector('.photo-url-error');
-const buttonSavePhoto = popupAddPhoto.querySelector('.form__btn-save')
-
-function openAddPhotoPopup() {
-  formAddPhoto.reset();
-  hideInputError(photoNameInput, photoNameErrorElement, configSelectorForm);
-  hideInputError(photoUrlInput, photoUrlErrorElement, configSelectorForm);
-  toggleButtonState(buttonSavePhoto, false, configSelectorForm);
-  openPopup(popupAddPhoto);
-}
-
-function addPhotoPopup(event) {
-  event.preventDefault();
-  const title = photoNameInput.value;
-  const url = photoUrlInput.value;
-  const cardData = {
-    name: title,
-    link: url
-  }
-  const cardElement = createCardElement(cardData);
-  galleryCardList.prepend(cardElement);
-  closePopup(popupAddPhoto);
-}
-
-export {
-  openPopup,
-  closePopup,
-  buttonEditProfile,
-  formSaveProfile,
-  openProfilePopup,
-  saveProfile,
-  buttonAddPhoto,
-  formAddPhoto,
-  openAddPhotoPopup,
-  addPhotoPopup
-};
+export {openPopup, closePopup};
--- src/index.js
+++ src/index.js
@@ -1,20 +1,74 @@
 import './pages/index.css';
-import {configSelectorForm} from './components/utils';
-import {
-  addPhotoPopup,
-  buttonAddPhoto,
-  buttonEditProfile,
-  formAddPhoto,
-  formSaveProfile,
-  openAddPhotoPopup,
-  openProfilePopup,
-  saveProfile
-} from './components/modal';
-import {fillGallery} from './components/card';
-import {enableValidation} from './components/validate';
+import {configSelectorForm, content, galleryCardList, page} from './components/utils';
+import {closePopup, openPopup,} from './components/modal';
+import {createCardElement, fillGallery} from './components/card';
+import {enableValidation, hideInputError, toggleButtonState} from './components/validate';
 
 // вставка изображений
 fillGallery();
+
+// попап редактирования профиля
+
+const popupProfile = page.querySelector('.popup_type_edit-profile');
+const buttonEditProfile = content.querySelector('.profile__btn-edit');
+const formSaveProfile = popupProfile.querySelector('.form');
+const profileNameInput = formSaveProfile.querySelector('.form__input_type_profile-name');
+const profileDescriptionInput = formSaveProfile.querySelector('.form__input_type_profile-description');
+const profileNameErrorElement = formSaveProfile.querySelector('.profile-name-error');
+const profileDescriptionErrorElement = formSaveProfile.querySelector('.profile-description-error');
+const buttonSaveProfile = popupProfile.querySelector('.form__btn-save')
+const profileNameElement = content.querySelector('.profile__title');
+const profileDescriptionElement = content.querySelector('.profile__subtitle');
+
+function openProfilePopup() {
+  profileNameInput.value = profileNameElement.textContent;
+  profileDescriptionInput.value = profileDescriptionElement.textContent;
+  hideInputError(profileNameInput, profileNameErrorElement, configSelectorForm);
+  hideInputError(profileDescriptionInput, profileDescriptionErrorElement, configSelectorForm);
+  toggleButtonState(buttonSaveProfile, false, configSelectorForm);
+  openPopup(popupProfile);
+}
+
+function saveProfile(event) {
+  event.preventDefault();
+  const name = profileNameInput.value;
+  const description = profileDescriptionInput.value;
+  profileNameElement.textContent = name;
+  profileDescriptionElement.textContent = description;
+  closePopup(popupProfile);
+}
+
+// попап добавления изображений
+
+const popupAddPhoto = page.querySelector('.popup_type_add-photo');
+const buttonAddPhoto = content.querySelector('.profile__btn-add');
+const formAddPhoto = popupAddPhoto.querySelector('.form');
+const photoNameInput = popupAddPhoto.querySelector('.form__input_type_photo-name');
+const photoUrlInput = popupAddPhoto.querySelector('.form__input_type_photo-url');
+const photoNameErrorElement = popupAddPhoto.querySelector('.photo-name-error');
+const photoUrlErrorElement = popupAddPhoto.querySelector('.photo-url-error');
+const buttonSavePhoto = popupAddPhoto.querySelector('.form__btn-save')
+
+function openAddPhotoPopup() {
+  formAddPhoto.reset();
+  hideInputError(photoNameInput, photoNameErrorElement, configSelectorForm);
+  hideInputError(photoUrlInput, photoUrlErrorElement, configSelectorForm);
+  toggleButtonState(buttonSavePhoto, false, configSelectorForm);
+  openPopup(popupAddPhoto);
+}
+
+function addPhotoPopup(event) {
+  event.preventDefault();
+  const title = photoNameInput.value;
+  const url = photoUrlInput.value;
+  const cardData = {
+    name: title,
+    link: url
+  }
+  const cardElement = createCardElement(cardData);
+  galleryCardList.prepend(cardElement);
+  closePopup(popupAddPhoto);
+}
 
 // слушатель на открытие попапа для добавление изображения
 buttonAddPhoto.addEventListener('click', openAddPhotoPopup);
